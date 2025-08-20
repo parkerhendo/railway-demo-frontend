a3b4b9eccc0f95a7b618b0152318a0b85a607bc5
Parker Henderson <parker@parkerhendo.com>
2025-08-20 11:02:25 -0700

    add another env var

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const API_BASE_URL = process.env.environment === "production" ? process.env.API_PRIVATE_URL : process.env.API_PUBLIC_URL;
 
+const API_AUTH_TOKEN = process.env.API_AUTH_TOKEN;
+
 export const API_USERS_URL = `${API_BASE_URL}/users`;
